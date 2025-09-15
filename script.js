3eb410054f7d6bc4e8f647a3b4c5542790832753
claeswdev <claeswdev@gmail.com>
2025-09-15 13:17:41 +0200

    Add hover effect on mouse enter

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const container = document.querySelector(".container");
 for (let i = 0; i < (16 * 16); i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
+    square.addEventListener("mouseenter", 
+        () => square.classList.toggle("hover"));
     container.appendChild(square);
 }
