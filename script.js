b8c78a5764be1c36c806384312cce76cb6c81664
claeswdev <claeswdev@gmail.com>
2025-09-16 09:57:37 +0200

    Add button for changing grid size
    You can now change the number of squares for a side with a button
    that prompts the user for a number no larger than 100.
    Larger number of squares makes the app slow.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
-for (let i = 0; i < (16 * 16); i++) {
-    const square = document.createElement("div");
-    square.classList.toggle("square");
-    square.addEventListener("mouseenter", 
-        () => square.classList.toggle("hover"));
-    container.appendChild(square);
+
+const setGridButton = document.createElement("button");
+setGridButton.textContent = "Set Grid";
+setGridButton.addEventListener("click", () => {
+    while (true) {
+        let userInput = prompt("Set the number of squares per side for the grid (max 100)", 16);
+        if (userInput === null) break;
+        if ((typeof parseInt(userInput) === "number") && ((userInput > 0) && (userInput <= 100))) {
+            setGrid(userInput);
+            break;
+        }
+    }
+});
+body.prepend(setGridButton);
+
+function setGrid(num) {
+    container.innerHTML = "";
+    for (let i = 0; i < (num * num); i++) {
+        const square = document.createElement("div");
+        const squareSize = container.clientWidth / num;
+        square.style.cssText = `width: ${squareSize}px; height: ${squareSize}px`;
+        square.classList.toggle("thin-dark-border");
+        square.addEventListener("mouseenter", 
+            () => square.classList.toggle("hover"));
+        container.appendChild(square);
+    }
 }
 
-const button = document.createElement("button");
-button.textContent = "Set Grid";
-button.addEventListener("click", () => {
-    let userInput = +prompt("Enter the size of a side for the grid (max 100)", 16);
-})
+setGrid(16);
